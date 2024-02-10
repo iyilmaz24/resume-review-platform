b4b80c90ad3c47b2befa3b148799d255cf998159
Irfan Yilmaz <iy.usa@outlook.com>
2024-02-10 04:19:59 -0500

    add error handling for userForm, improve UX and styling
    add error handling to prevent blank input submissions
    turn blank inputs red show the user where the error is
    once blank inputs are filled, when submit button clicked, remove red color
    use setTimeout to improve UX after successful submission

--- frontend/src/components/formInput.jsx
+++ frontend/src/components/formInput.jsx
@@ -4,13 +4,19 @@ import { IconAt } from '@tabler/icons-react';
 import { TextInput } from '@mantine/core';
 
 
-function FormInput( { defaultText, eHandler} ) {
+function FormInput( { defaultText, eHandler, eMsg} ) {
 
     return (
 
-        <TextInput size='sm' placeholder={defaultText}
-        onChange={(e) => eHandler(e.currentTarget.value)} 
-        leftSection={<IconAt size={16} required />} />
+        (defaultText == eMsg) ?
+          <TextInput size='sm' placeholder={"Invalid " + defaultText + " @"}
+          onChange={(e) => eHandler(e.currentTarget.value)} 
+          leftSection={<IconAt size={16} required />} error/>
+        :
+          <TextInput size='sm' placeholder={"Your " + defaultText + " @"}
+          onChange={(e) => eHandler(e.currentTarget.value)} 
+          leftSection={<IconAt size={16} required />} />
+
 
     );
   }
--- frontend/src/pages/UserForm.jsx
+++ frontend/src/pages/UserForm.jsx
@@ -1,34 +1,59 @@
 import { useState } from 'react';
 import uploadFileName from '../scripts/fileName';
 import FormInput from '../components/formInput';
-
-
-
-import { Link } from 'react-router-dom';
 import { Button } from '@mantine/core';
 import { IconSend2 } from '@tabler/icons-react';
+import { Navigate } from "react-router-dom";
+
 
 function UserForm() {
 
   const [discordAt, setDiscordAt] = useState("");
   const [instagramAt, setInstagramAt] = useState("");
-
   const [fileName, setFileName] = useState("No File Selected.");
-  const [fileBtnText, setFileBtnText] = useState("Upload Resume");
 
+  const [fileBtnText, setFileBtnText] = useState("Upload Resume");
   const [isUploading, setIsUploading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const [pageTitle, setPageTitle] = useState("Upload a Resume");
+  const [toHome, setToHome] = useState(false);
+  if (toHome === true) {
+    return <Navigate to="/" />;
+  };
 
   async function handleSubmit() {
+      if(discordAt == "" || instagramAt == "") {
+        setErrorMsg( (discordAt == "" ? "Discord" : 
+        "Instagram") );
+        return;
+      };
+      setErrorMsg("");
+
+      if(fileName == "No File Selected.") {
+        document.getElementById("fileNameDisplay").style.color = "red";
+        return;
+      }
+      document.getElementById("fileNameDisplay").style.color = "";
+
+      
       setIsUploading(true);
+      setPageTitle("Thanks!");
+
       fetch("http://localhost:3001/users")
       .then(res => console.log(res.json()));
         // .then(res => res.json())
         // .then(res => console.log(JSON.stringify(res)));
+      
+      // if request fails setPageTitle to retry submit and allow re-submission of form
+      // setPageTitle("Retry Submission")
+      // can also turn setPageTitle text dark red
+      setTimeout(() => {setToHome(true)}, 2000)
   }
 
   return ( <>
       
-    <h1 className="title-style">Upload a Resume</h1>
+    <h1 className="title-style">{pageTitle}</h1>
 
     {/* <input type='file' className='hideItem'></input> */}
     {/* <input type='file'></input>
@@ -37,9 +62,9 @@ function UserForm() {
     <form className='min-w-screen min-h-fit 
     flex-row place-items-evenly space-y-4 p-4'>
       
-        <FormInput defaultText="Your Discord" eHandler={setDiscordAt}/>
+        <FormInput defaultText="Discord" eHandler={setDiscordAt} eMsg={errorMsg}/>
 
-        <FormInput defaultText="Your Instagram" eHandler={setInstagramAt}/>
+        <FormInput defaultText="Instagram" eHandler={setInstagramAt} eMsg={errorMsg}/>
 
         {/* <h4>{discordAt}</h4>
         <h4>{instagramAt}</h4> */}
@@ -54,7 +79,7 @@ function UserForm() {
         <input id="file-upload" type="file" className='hideItem'
               onChange={(e) => uploadFileName(e, setFileName, setFileBtnText)}/>
 
-        <h4>{fileName}</h4>
+        <h4 id='fileNameDisplay'>{fileName}</h4>
       
       <div className='p-16'>
         {isUploading === true ?
@@ -67,7 +92,6 @@ function UserForm() {
               Submit Info&nbsp;<IconSend2 size={18}/>
           </Button> }
       </div>
-
 
     </form>
 
